785e37f0a1dd25dc7d039eb9574f623de7b7dc9d
zGumeloBr <66852780+zGumeloBr@users.noreply.github.com>
2021-11-10 19:59:00 -0300

    Create entrypoint.sh

---
+++ gate/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /home/container
+
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+${MODIFIED_STARTUP}
