c102475e1352aad700c1626fcb4e5b6a6074ae55
zGumeloBr <66852780+zGumeloBr@users.noreply.github.com>
2021-11-10 19:04:52 -0300

    Update entrypoint.sh

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -4,6 +4,6 @@ cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo -e "\033[1;32m:/home/container$ ${MODIFIED_STARTUP}\033[0m"
+echo -e "\033[0;32mSTARTUP: ${MODIFIED_STARTUP}\033[0m"
 
 eval ${MODIFIED_STARTUP}
