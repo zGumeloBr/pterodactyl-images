b8b4ff6fbf7ef60d15a5d8bf26ae6e201f92d787
zGumeloBr <yan.mf@outlook.com>
2024-05-07 01:26:31 -0300

    Mirror with main

--- java/graalvm/entrypoint.sh
+++ java/graalvm/entrypoint.sh
@@ -3,7 +3,8 @@ cd /home/container
 
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+java -version
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo -e "\033[0;32mSTARTUP: ${MODIFIED_STARTUP}\033[0m"
+echo -e "\033[0;36mSTARTUP: ${MODIFIED_STARTUP}\033[0m"
 
 eval ${MODIFIED_STARTUP}
