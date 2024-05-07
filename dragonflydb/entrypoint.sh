b30430690b47de228a00542773fe0a776a39f6fb
zGumeloBr <yan.mf@outlook.com>
2024-05-07 01:18:55 -0300

    Added terminal color in dragonfly

--- dragonflydb/entrypoint.sh
+++ dragonflydb/entrypoint.sh
@@ -2,5 +2,6 @@
 cd /home/container
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo -e "\033[0;36mSTARTUP: ${MODIFIED_STARTUP}\033[0m"
 
 ${MODIFIED_STARTUP}
