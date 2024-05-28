845b3bd2cfba69d1a3803848bd46254b3fd08db0
zGumeloBr <yan.mf@outlook.com>
2024-05-28 15:13:00 -0300

    Fix terminal color

--- java/zulu/entrypoint.sh
+++ java/zulu/entrypoint.sh
@@ -5,6 +5,6 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 java -version
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo -e "\036[0;32mSTARTUP: ${MODIFIED_STARTUP}\033[0m"
+echo -e "\033[0;36mSTARTUP: ${MODIFIED_STARTUP}\033[0m"
 
 eval ${MODIFIED_STARTUP}
